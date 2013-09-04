0027fddd2012812a6b8103a5596a52090943aa7a
cbonfa <bonfa@inaum.net>
2013-09-04 14:26:50 -0300

    Update image.rb

--- lib/bootsy/activerecord/image.rb
+++ lib/bootsy/activerecord/image.rb
@@ -3,6 +3,8 @@ module Bootsy
     belongs_to :image_gallery, touch: true
 
     mount_uploader :image_file, ImageUploader
+    
+    attr_accessible :bootsy_resource_id, :bootsy_resource_type
 
     validates_presence_of :image_file, :image_gallery_id
   end
