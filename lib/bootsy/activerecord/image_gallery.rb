31e850f16b3c29a1427fb130f7a48b545e85358f
cbonfa <bonfa@inaum.net>
2013-09-04 14:28:28 -0300

    Update image_gallery.rb

--- lib/bootsy/activerecord/image_gallery.rb
+++ lib/bootsy/activerecord/image_gallery.rb
@@ -2,6 +2,9 @@ module Bootsy
   class ImageGallery < ActiveRecord::Base
     belongs_to :bootsy_resource, polymorphic: true
     has_many :images, dependent: :destroy
+    accepts_nested_attributes_for :images, :allow_destroy => true
+    
+    attr_accessible :bootsy_resource_id, :bootsy_resource_type
 
     scope :destroy_orphans, ->(time_limit) { where('created_at < ? AND bootsy_resource_id IS NULL', time_limit).destroy_all }
   end
